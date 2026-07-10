13a5df93141a9f918c9ed7e711a14b369fb10e0b
Özgür Ulukan <ulukano@icloud.com>
2026-07-11 02:39:19 +0300

    Update chat tab icon to sparkles

--- BabyUltra/Views/MainTabView.swift
+++ BabyUltra/Views/MainTabView.swift
@@ -11,7 +11,7 @@ enum BabyUltraTab: String, CaseIterable {
         switch self {
         case .home:        return "house.fill"
         case .generations: return "square.grid.2x2.fill"
-        case .chatEdit:    return "textformat.abc"
+        case .chatEdit:    return "sparkles"
         case .account:     return "person.fill"
         }
     }
